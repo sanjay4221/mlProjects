4bd01b63fa52d0a84107c3fddf68bf73cf9aee1a
Sanjay Kumar <sanjayetc.kumar008@gmail.com>
2025-09-06 11:59:00 +1200

    data transaformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,7 +5,8 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import data_transformation_config
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 # used to class variables in short dataclasses
@@ -52,6 +53,12 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_path, test_path = obj.initiate_data_ingestion()
-    print(f"Train data saved at: {train_path}")
-    print(f"Test data saved at: {test_path}")
+    train_data, test_data = obj.initiate_data_ingestion()
+    print(f"Train data saved at: {train_data}")
+    print(f"Test data saved at: {test_data}")
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+    logging.info("Obtained preprocessing object")
+
+
